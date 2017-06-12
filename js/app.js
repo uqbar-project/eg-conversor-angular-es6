20b3114a17172b06e0f4c8a49f40fbed7fbf65a0
fdodino <fernando.dodino@gmail.com>
2017-06-12 18:20:00 -0300

    Replace var -> let

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 (() => {
     'use strict';
 
-    var app = angular.module('conversorApp', [])
+    let app = angular.module('conversorApp', [])
 
     /* Controllers */
     app.controller('conversorCtrl', () => {
