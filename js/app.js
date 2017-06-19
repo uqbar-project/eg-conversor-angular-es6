18b2c3f628f248de57aeb850ea0b0737dfd5d20e
fdodino <fernando.dodino@gmail.com>
2017-06-19 17:13:08 -0300

    Removing unnecessary ()

--- js/app.js
+++ js/app.js
@@ -1,21 +1,16 @@
-(() => {
-    'use strict';
+const app = angular.module('conversorApp', [])
 
-    let app = angular.module('conversorApp', [])
+/* Controller */
+app.controller('conversorCtrl', () => {
+    return new Conversor()
+})
 
-    /* Controller */
-    app.controller('conversorCtrl', () => {
-        return new Conversor()
-    })
-
-    /** Filter, muestra un número con coma decimal */
-    app.filter('numero', () => {
-        return (input) => {
-            if (input === null || isNaN(input)) {
-                return "Ingrese un número"
-            }
-            return input.toString().replace('.', ',')
+/** Filter, muestra un número con coma decimal */
+app.filter('numero', () => {
+    return (input) => {
+        if (input === null || isNaN(input)) {
+            return "Ingrese un número"
         }
-    })
-
-})()
+        return input.toString().replace('.', ',')
+    }
+})
